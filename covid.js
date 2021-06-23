326280b196eed5ed04249b833ca4bd34e0f1d58a
Danish Qureshi <85394659+Danishqureshi8817@users.noreply.github.com>
2021-06-23 12:01:18 +0530

    Update covid.js

--- covid.js
+++ covid.js
@@ -1,8 +1,4 @@
-function show(){
-    var an=area.options[area.selectedIndex].text
-    areaname.innerHTML="&#10148;"+"<b>AREANAME:"+an+"</b>"
-    tdose.innerHTML="&#10148;"+"<b>TOTAL AVAILABLE DOSE:"+area.options[area.selectedIndex].value+"</b>"
-}
+
 function vaccine(){
     var vv=vac.options[vac.selectedIndex].value
     var vn=vac.options[vac.selectedIndex].text
@@ -16,4 +12,8 @@ function subtract(){
     var tr=rgst.value-ad
     rdose.innerHTML="&#10148;"+"<b>REQUIRED DOSE:"+tr+"</b>"
     demand.innerHTML="&#10148;"+"<b>TOTAL DEMAND:"+rgst.value+"</b>"
+
+    var an=area.options[area.selectedIndex].text
+    areaname.innerHTML="&#10148;"+"<b>AREANAME:"+an+"</b>"
+    tdose.innerHTML="&#10148;"+"<b>TOTAL AVAILABLE DOSE:"+area.options[area.selectedIndex].value+"</b>"
 }
